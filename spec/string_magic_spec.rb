813b7236e7ab87603c3c3f4e9614dc329db23718
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-11-22 16:29:47 -0300

    test(snake-case): cobre edge cases e strings com acentos.

--- spec/string_magic_spec.rb
+++ spec/string_magic_spec.rb
@@ -2,28 +2,74 @@ require "string_magic"
 
 RSpec.describe StringMagic do
   describe ".to_snake_case" do
-    it "converte camelCase para snake_case" do
-      expect(StringMagic.to_snake_case("userName")).to eq("user_name")
+    context "conversões básicas" do
+      it "converte camelCase para snake_case" do
+        expect(StringMagic.to_snake_case("userName")).to eq("user_name")
+      end
+
+      it "converte PascalCase para snake_case" do
+        expect(StringMagic.to_snake_case("UserName")).to eq("user_name")
+      end
+
+      it "lida com strings que contêm espaços" do
+        expect(StringMagic.to_snake_case("user name")).to eq("user_name")
+      end
+
+      it "lida com strings com várias letras maiúsculas" do
+        expect(StringMagic.to_snake_case("getUserHTTPResponse")).to eq("get_user_http_response")
+      end
+
+      it "lida com strings em snake_case" do
+        expect(StringMagic.to_snake_case("user_name")).to eq("user_name")
+      end
     end
 
-    it "converte PascalCase para snake_case" do
-      expect(StringMagic.to_snake_case("UserName")).to eq("user_name")
+    context "edge cases" do
+      it "lida com strings vazias" do
+        expect(StringMagic.to_snake_case("")).to eq("")
+      end
+      
+      it "lida com valores nulos(nil)" do
+        expect(StringMagic.to_snake_case(nil)).to eq("")
+      end
+
+      it "lida com um único caractere" do
+        expect(StringMagic.to_snake_case("A")).to eq("a")
+      end
+
+      it "lida com strings e números" do
+        expect(StringMagic.to_snake_case("user123Name")).to eq("user123_name")
+      end
+
+      it "lida com strings começando com números" do
+        expect(StringMagic.to_snake_case("123userName")).to eq("123user_name")
+      end
+
+      it "lida com múltiplas letras maiúsculas" do
+        expect(StringMagic.to_snake_case("XMLHttpRequest")).to eq("xml_http_request")
+      end
+
+      it "lida com strings com hífens" do
+        expect(StringMagic.to_snake_case("user-name-test")).to eq("user_name_test")
+      end
+
+      it "lida com strings com vários espaços" do
+        expect(StringMagic.to_snake_case("user   name")).to eq("user_name")
+      end
+
+      it "lida com strings com separadores mistos" do
+        expect(StringMagic.to_snake_case("user-name test_data")).to eq("user_name_test_data")
+      end
     end
 
-    it "lida com strings que contêm espaços" do
-      expect(StringMagic.to_snake_case("user name")).to eq("user_name")
-    end
+    context "casos com acentos" do
+      it "preserva caracteres acentuados" do
+        expect(StringMagic.to_snake_case("nomeUsuário")).to eq("nome_usuário")
+      end
 
-    it "lida com strings com várias letras maiúsculas" do
-      expect(StringMagic.to_snake_case("getUserHTTPResponse")).to eq("get_user_http_response")
-    end
-
-    it "lida com strings em snake_case" do
-      expect(StringMagic.to_snake_case("user_name")).to eq("user_name")
-    end
-
-    it "lida com strings vazias" do
-      expect(StringMagic.to_snake_case("")).to eq("")
+      it "lida com caracteres acentuados maiúsculos" do
+        expect(StringMagic.to_snake_case("nomeÚltimo")).to eq("nome_último")
+      end
     end
   end
 
